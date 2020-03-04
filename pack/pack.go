20ae34706cd247670681a48fe8e53dc88ee9a0ed
Ilya Mateyko <me@astrophena.me>
2020-03-04 18:48:14 +0300

    Add stub version of pack package

---
+++ pack/pack.go
@@ -0,0 +1,7 @@
+// © 2020 Ilya Mateyko. All rights reserved.
+// © 2019 Frédéric Guillot. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE.md file.
+
+// Package pack implements asset bundling.
+package pack // import "astrophena.me/gen/pack"
