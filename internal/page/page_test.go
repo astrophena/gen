85ff407e9ac9e49f37a50f9621f3fa8df95f8b8e
Ilya Mateyko <me@astrophena.me>
2020-04-08 22:44:00 +0300

    Add a basic test for the page package

---
+++ internal/page/page_test.go
@@ -0,0 +1,51 @@
+// © 2020 Ilya Mateyko. All rights reserved.
+// © 2019 Frédéric Guillot. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE.md file.
+
+package page_test
+
+import (
+	"fmt"
+	"html/template"
+	"path/filepath"
+	"reflect"
+	"testing"
+
+	"go.astrophena.me/gen/internal/page"
+)
+
+func testTpl(t *testing.T) *template.Template {
+	tpls := []string{filepath.Join("testdata", "layout.html")}
+
+	tpl, err := page.ParseTemplates(page.Template(), tpls)
+	if err != nil {
+		t.Error(err)
+	}
+
+	return tpl
+}
+
+func TestValidParse(t *testing.T) {
+	tpl := testTpl(t)
+	f := filepath.Join("testdata", "valid.md")
+
+	p, err := page.Parse(tpl, f)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Keep this updated with the testdata/valid.md.
+	expected := &page.Page{
+		URI:      "index.html",
+		Content:  fmt.Sprintf("<p>Hello, world!</p>\n"),
+		Title:    "Hello, world!",
+		MetaTags: make(map[string]string),
+		Template: "layout",
+	}
+	parsed := p
+
+	if !reflect.DeepEqual(expected, parsed) {
+		t.Errorf("expected %v, but parsed %v", expected, parsed)
+	}
+}
