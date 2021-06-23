24255159603d6fd0d59e0b67775349a928c02d82
Ilya Mateyko <me@astrophena.name>
2021-06-24 00:43:27 +0300

    site: use t.Fatalf instead of t.Errorf in test

--- site/site_test.go
+++ site/site_test.go
@@ -11,29 +11,30 @@ import (
 )
 
 func TestBuild(t *testing.T) {
-	log.SetOutput(io.Discard) // Silence logs.
+	// Silence log output.
+	log.SetOutput(io.Discard)
 
 	src, err := os.MkdirTemp("", "gen-site-test-src")
 	if err != nil {
-		t.Errorf("Failed to create a temporary directory: %v", err)
+		t.Fatalf("Failed to create a temporary directory: %v", err)
 	}
 	defer os.RemoveAll(src)
 	dst, err := os.MkdirTemp("", "gen-site-test-dst")
 	if err != nil {
-		t.Errorf("Failed to create a temporary directory: %v", err)
+		t.Fatalf("Failed to create a temporary directory: %v", err)
 	}
 	defer os.RemoveAll(dst)
 
 	if err := scaffold.Create(src); err != nil {
-		t.Errorf("Failed to generate a new site: %v", err)
+		t.Fatalf("Failed to generate a new site: %v", err)
 	}
 
 	s, err := site.New(src, dst, false)
 	if err != nil {
-		t.Errorf("Failed to initialize a new site: %v", err)
+		t.Fatalf("Failed to initialize a new site: %v", err)
 	}
 
 	if err := s.Build(); err != nil {
-		t.Errorf("Failed to build a site: %v", err)
+		t.Fatalf("Failed to build a site: %v", err)
 	}
 }
